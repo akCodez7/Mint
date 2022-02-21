432e65cd96b67c3dbc7391e59f5914cbe3a4649c
Ayan <99747341+akCodez7@users.noreply.github.com>
2022-02-22 00:57:07 +0500

    Create repo.cmd

---
+++ repo.cmd
@@ -0,0 +1,6 @@
+$nomention
+$title[GitHub Repo]
+$description[
+Click [here](https://github.com/akCodez7/Mint/tree/main) for the official GitHub repo.]
+$footer[Support is provided in our support server.]
+$color[0ffe5]
