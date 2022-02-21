fe46300c2657adcee00e745c4f36a5afdf1b79f9
Ayan <99747341+akCodez7@users.noreply.github.com>
2022-02-22 00:40:29 +0500

    Create help.cmd

---
+++ help.cmd
@@ -0,0 +1,17 @@
+$nomention
+$title[Mint Support]
+$description[
+
+**new**
+Add a new command.
+
+**repo**
+View the official GitHub repo.
+
+**support**
+Support for the bot usage.]
+$footer[Feel free to execute any command.]
+$color[0ffe4]
+$c[SOURCE CODE USERS!
+Requirements:
+None]
