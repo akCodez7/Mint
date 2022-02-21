bc5663b069f42f6f7e8a7f7f00e79c06f24afe02
Ayan <99747341+akCodez7@users.noreply.github.com>
2022-02-22 01:02:15 +0500

    Create support.cmd

---
+++ support.cmd
@@ -0,0 +1,6 @@
+$nomention
+$title[Support Server]
+$description[
+Click [here](https://discord.gg/vevdQyEbtt) for the official support server.]
+$footer[We only have this support server, Please note we do not have another support server and we do not ask for your password or ask you to pay for a "subscription".]
+$color[0ffe4]
